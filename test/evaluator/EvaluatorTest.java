e63f862c574df0584438babd3809c02e1ab21ead
daveborrel <dborrel@student.ubc.ca>
2024-02-07 20:55:59 -0800

    added more structure to evaluator tests

--- test/evaluator/EvaluatorTest.java
+++ test/evaluator/EvaluatorTest.java
@@ -2,7 +2,13 @@ package evaluator;
 
 import ast.Note;
 import ast.evaluator.Evaluator;
+import org.antlr.v4.runtime.CharStreams;
 import org.junit.Test;
+import parser.MusicLanguageLexer;
+import parser.MusicLanguageParser;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,7 +21,19 @@ public class EvaluatorTest {
         // Note input = new Note("c", "sharp", "4");
         // File expected_out = new File("text_output.ly");
 
+        // MusicLanguageLexer lexer = new MusicLanguageLexer(CharStreams.fromString(input));
+        // lexer.reset();
+        // MusicLanguageParser parser = new MusicLanguageParser(lexer);
+
+        // visitor
+        // Node parsedProgram = parser.program().accept(visitor);
+
+        // StringWriter swOut = new StringWriter();
+        // PrintWriter pwOut = new PrintWriter(swOut);
         // Evaluator e = new Evaluator();
+        // parsedProgram.accept(e, pwOut);
+        // pwOut.close();
+        // String out = swOut.toString();
         // assertEquals(expected_out, out);
     }
 
@@ -27,7 +45,19 @@ public class EvaluatorTest {
         // Note input = new Note("c", "sharp", "4");
         // File expected_out = new File("text_output.ly");
 
+        // MusicLanguageLexer lexer = new MusicLanguageLexer(CharStreams.fromString(input));
+        // lexer.reset();
+        // MusicLanguageParser parser = new MusicLanguageParser(lexer);
+
+        // visitor
+        // Node parsedProgram = parser.program().accept(visitor);
+
+        // StringWriter swOut = new StringWriter();
+        // PrintWriter pwOut = new PrintWriter(swOut);
         // Evaluator e = new Evaluator();
+        // parsedProgram.accept(e, pwOut);
+        // pwOut.close();
+        // String out = swOut.toString();
         // assertEquals(expected_out, out);
     }
 }
