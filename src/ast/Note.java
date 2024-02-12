3582312cb4e17a6003da4a54d356a0ef1b8c0d37
William <wgumboc@gmail.com>
2024-02-12 12:35:03 -0800

    Implemented setters for Note fields.

--- src/ast/Note.java
+++ src/ast/Note.java
@@ -3,9 +3,9 @@ package ast;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Note extends Node{
-    private final String key;
-    private final String pitch;
-    private final String beat;
+    private String key;
+    private String pitch;
+    private String beat;
 
     public Note(String key, String beat, String pitch) {
         this.key = key;
@@ -24,6 +24,19 @@ public class Note extends Node{
     public String getBeat() {
         return this.beat;
     }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setPitch(String pitch) {
+        this.pitch = pitch;
+    }
+
+    public void setBeat(String beat) {
+        this.beat = beat;
+    }
+
 
     // This needs to be deleted later
     public String printNote() {
